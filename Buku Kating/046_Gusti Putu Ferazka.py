c0fdf489de148e078f2cf08c72ea1e308a3b76ad
ferazka <ferazkaa@gmail.com>
2024-10-14 13:04:55 +0700

    sekjen

--- Buku Kating/046_Gusti Putu Ferazka.py
+++ Buku Kating/046_Gusti Putu Ferazka.py
@@ -99,11 +99,12 @@ menu = streamlit_menu()
 if menu == "Kesekjenan":
     def kesekjenan():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1qYxu5aboj2krXZ5yKDz8uK8scMH_nfhI",
-            "https://drive.google.com/uc?export=view&id=1xz8K2U5Q98yu4F_BiAh1R_9UaW-lRmBE",
+            "https://drive.google.com/uc?export=view&id=1FlkHgMsYz-989GVpwbv61n1UUwPkjZlv",
+            "https://drive.google.com/uc?export=view&id=1FgF1JmbqCHIii07WC1w0fWOZ0VmmHdTJ",
+            "https://drive.google.com/uc?export=view&id=1FcC8_i9v7OLYC6B9JM8b1C_F8yklpmCv",
+            "https://drive.google.com/uc?export=view&id=1FvWmr0pCYUWiMhPsuOI6uHCud1f1NiNp",
+            "https://drive.google.com/uc?export=view&id=1FeHQKQlMPa5bTi7he-96h6psI",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-        
         ]
         data_list = [
             {
